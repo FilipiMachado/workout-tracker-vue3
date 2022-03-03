a06806fa03010f805bbf00aac4c1f8063dc44a9c
filipi <filipisee@hotmail.com>
2022-03-02 21:49:09 -0300

    creating base structure for App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <div>
+  <div class="min-h-full font-Poppins box-border">
+    <Navigation />
     <router-view />
   </div>
 </template>
 
 <script>
+import Navigation from "./components/Navigation.vue"
+
 export default {
+  components: {
+    Navigation
+  },
   setup() {
     // Create data / vars
 
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,5 +1,15 @@
 <template>
-  <header></header>
+  <header class="bg-at-light-green text-white">
+    <nav class="container py-5 px-4 flex flex-col gap-4 items-center sm:flex-row">
+      <div class="flex items-center gap-x-4">
+        <img class="w-14" src="../assets/images/dumbbell-light.png" alt="">
+        <h1 class="text-lg">Active Tracker</h1>
+      </div>
+      <ul class="flex flex-1 justify-end gap-x-10">
+        <router-link class="cursor-pointer" :to="{ name: 'Home' }">Home</router-link>
+      </ul>
+    </nav>
+  </header>
 </template>
 
 <script>
