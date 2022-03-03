1a564da7c07b8cfc8f75500503a2d088850228cf
filipi <filipisee@hotmail.com>
2022-03-03 09:47:09 -0300

    adding logic to login page

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -32,7 +32,7 @@
                id="confirmPassword"
                v-model="confirmPassword">
       </div>
-      <button type="submit" class="mt-6 py-2 px-6 rounded-sm self-start text-sm text-white bg-at-light-green duration-200 border-solid border-2 border-transparent hover:border-white hover:bg-white hover:text-at-light-green">Confirm Password</button>
+      <button type="submit" class="mt-6 py-2 px-6 rounded-sm self-start text-sm text-white bg-at-light-green duration-200 border-solid border-2 border-transparent hover:border-at-light-green hover:bg-white hover:text-at-light-green">Confirm Password</button>
       <router-link class="text-sm mt-6 text-center" :to="{name: 'Login'}">
         Already have an account? <span class="text-at-light-green">Login</span>
       </router-link>
@@ -44,7 +44,7 @@
 import { ref } from "vue"
 
 export default {
-  name: "register",
+  name: "Register",
   setup() {
 
     // Create data / vars
