3cb3b7d6fd55b35d1ac528fbb4142563871db9e0
filipi <filipisee@hotmail.com>
2022-03-02 23:13:56 -0300

    finishing register page and routes

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -7,6 +7,9 @@
       </div>
       <ul class="flex flex-1 justify-end gap-x-10">
         <router-link class="cursor-pointer" :to="{ name: 'Home' }">Home</router-link>
+        <router-link class="cursor-pointer" :to="{ name: '' }">Create</router-link>
+        <router-link class="cursor-pointer" :to="{ name: 'Login' }">Login</router-link>
+        <li class="cursor-pointer">Logout</li>
       </ul>
     </nav>
   </header>
